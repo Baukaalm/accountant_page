efd46fc54e3c8e5b3b48e19c703318217dad0f2f
Baurzhan <baur3dn@gmail.com>
2017-06-04 22:09:03 +0600

    changes checkoboxes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,28 @@
-$(document).ready(function(){
-   $('.parallax').parallax();
-   $('select').material_select();
- });
+$(document).ready(function() {
+  $('.parallax').parallax();
+  $('select').material_select();
+});
 
 
-const elements  = {
-  documents:$('.js-documents'),
-  participants:$('.js-participants'),
-  price:$('.js-pirce')
+const elements = {
+  documents: $('.js-documents'),
+  participants: $('.js-participants'),
+  price: $('.js-pirce')
 };
+const checkboxes = {
+  param_too: $('#too'),
+  param_ip:  $('#ip'),
+  param_usn: $('#usn'),
+  param_nds: $('#nds')
+}
 
 const values = {
-  documentsValue:20,
-  participantsValue:2,
+  documentsValue: 20,
+  participantsValue: 2,
   price: null
 }
+
+
 
 
 
@@ -23,54 +31,54 @@ const sliderDocs = document.querySelector('.js-slider-documents');
 const documentFormat = wNumb({
   decimals: 0,
 });
- noUiSlider.create(sliderDocs, {
+noUiSlider.create(sliderDocs, {
   start: values.documentsValue,
-  connect:'lower',
+  connect: 'lower',
   step: 25,
   range: {
     'min': 25,
     'max': 300
   },
-  format:documentFormat
- });
+  format: documentFormat
+});
 
- // participants slider
+// participants slider
 
- const sliderParticipants = document.querySelector('.js-slider-participants');
- const formatParticipants = wNumb({
-   decimals: 0,
-   thousand: ',',
- });
-  noUiSlider.create(sliderParticipants, {
-   start: values.participantsValue,
-   connect:'lower',
-   step: 1,
-   range: {
-     'min': 2,
-     'max': 20
-   },
-   format:formatParticipants
-  });
+const sliderParticipants = document.querySelector('.js-slider-participants');
+const formatParticipants = wNumb({
+  decimals: 0,
+  thousand: ',',
+});
+noUiSlider.create(sliderParticipants, {
+  start: values.participantsValue,
+  connect: 'lower',
+  step: 1,
+  range: {
+    'min': 2,
+    'max': 20
+  },
+  format: formatParticipants
+});
 
 
-  sliderDocs.noUiSlider.on('update', function(values,handle) {
-    let value = parseInt(values[handle]);
-    values.documentsValue = value;
-    console.log(values.price);
-    console.log(values.documentsValue);
-    elements.documents.text(values);
-    if (value === 300) {
-      elements.documents.text(values.documentsValue + ' и выше');
-    }
-  });
+sliderDocs.noUiSlider.on('update', function(values, handle) {
+  let value = parseInt(values[handle]);
+  values.documentsValue = value;
+  console.log(values.price);
+  console.log(values.documentsValue);
+  elements.documents.text(values);
+  if (value === 300) {
+    elements.documents.text(values.documentsValue + ' и выше');
+  }
+});
 
-  sliderParticipants.noUiSlider.on('update', function(values, handle) {
-    let value = parseInt(values[handle]);
-    values.participantsValue = value;
-    console.log(values.participantsValue);
-    elements.participants.text(values);
-    if (value === 20) {
-      elements.participants.text(values.participantsValue + ' и выше');
-    }
+sliderParticipants.noUiSlider.on('update', function(values, handle) {
+  let value = parseInt(values[handle]);
+  values.participantsValue = value;
+  console.log(values.participantsValue);
+  elements.participants.text(values);
+  if (value === 20) {
+    elements.participants.text(values.participantsValue + ' и выше');
+  }
 
-  });
+});
